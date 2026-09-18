04fe5257da195b4b4e9084231db2173c9db380fa
Adarsh Kumar Singh <155250714+ADARSH010203@users.noreply.github.com>
2026-09-18 14:26:50 -0400

    config: add A2A input size guardrail

--- app/config/settings.py
+++ app/config/settings.py
@@ -34,6 +34,7 @@ class Settings:
     a2a_api_key: str = os.getenv("A2A_API_KEY", "")
     a2a_rate_limit_per_minute: int = int(os.getenv("A2A_RATE_LIMIT_PER_MINUTE", "60"))
     a2a_max_concurrent_tasks: int = int(os.getenv("A2A_MAX_CONCURRENT_TASKS", "8"))
+    a2a_max_input_chars: int = int(os.getenv("A2A_MAX_INPUT_CHARS", "20000"))
     push_notification_require_https: bool = _env_bool(
         "PUSH_NOTIFICATION_REQUIRE_HTTPS",
         True,
