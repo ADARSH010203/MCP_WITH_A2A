6f83e790f17c61cb9e2969f00794ce7a3919e84c
Adarsh Kumar Singh <155250714+ADARSH010203@users.noreply.github.com>
2026-09-18 12:20:50 -0400

    refactor: add package structure for app/config/constants.py

---
+++ app/config/constants.py
@@ -0,0 +1,17 @@
+SUPPORTED_CONTENT_TYPES = [
+        "text", "text/plain", "application/json",
+        "image/png", "image/jpeg", "image/gif", "image/webp", "image/svg+xml",
+        "application/python", "application/javascript", "application/typescript",
+        "application/java", "application/c", "application/cpp", "application/csharp",
+        "application/go", "application/rust", "application/ruby", "application/swift",
+        "application/kotlin", "application/scala", "application/dart", "application/php",
+        "application/perl", "application/r", "application/julia", "application/haskell",
+        "application/lua", "application/bash", "application/shell", "application/html",
+        "application/css", "application/sql", "application/nosql", "application/xml",
+        "application/yaml", "application/toml", "application/markdown", "application/objective-c",
+        "application/groovy", "application/vbnet", "application/assembly", "application/fortran",
+        "application/fsharp", "application/ocaml", "application/elixir", "application/clojure",
+        "application/gdscript", "application/solidity", "application/vhdl", "application/verilog",
+        "application/apex", "application/prolog", "application/nim", "application/zig",
+        "application/tcl", "application/scheme"
+    ]
